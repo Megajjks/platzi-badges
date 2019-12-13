ac9058f682b05198b2569436ab45e2753e0bea8b
Jayro_Salazar <megajjks@gmail.com>
2019-12-13 13:30:05 -0600

    last change

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Layout from '../components/Layout'
@@ -24,29 +23,3 @@ function App(){
 }
 
 export default App;
-=======
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import Layout from './Layout';
-import Home from '../pages/Home';
-import Badges from '../pages/Badges';
-import BadgeNew from '../pages/BadgeNew';
-import NotFound from '../pages/NotFound';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/badges" component={Badges} />
-          <Route exact path="/badges/new" component={BadgeNew} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
-  );
-}
-
-export default App;
->>>>>>> 7ad536f94341d1cd0e2f9b470b3dee61de11345e
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './components/App'
 import 'bootstrap/dist/css/bootstrap.css'
 import './global.css'
@@ -10,13 +9,3 @@ const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(<App/>, container);
-=======
-import 'bootstrap/dist/css/bootstrap.css';
-
-import './global.css';
-import App from './components/App';
-
-const container = document.getElementById('app');
-
-ReactDOM.render(<App />, container);
->>>>>>> 7ad536f94341d1cd0e2f9b470b3dee61de11345e
